f3750cbd0f748915658bab809294d781246dd604
Yami6575 <nikhil.mishra112004@gmail.com>
2023-12-25 01:59:36 +0530

    readmechanges

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
     
     return (
         <header>
-            <Link className="site-logo" to="/">#VanLife</Link>
+            <Link className="site-logo" to="/">Van Rental</Link>
             <nav>
                 
                 <NavLink 
